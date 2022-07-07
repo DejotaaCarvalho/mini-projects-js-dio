cbbbe9710476bfdcea704e0751726f6922745df8
Daniel Carvalho <83430928+DejotaaCarvalho@users.noreply.github.com>
2022-07-07 15:12:20 -0300

    Adicionando exercicio sem o this

---
+++ 01 Map-filter-reduce/map/mapSemThis.js
@@ -0,0 +1,9 @@
+// MapSemThis
+
+function mapSemThis(arr) {
+    return arr.map((item) => item *2);
+}
+
+const array = [2, 4, 6, 8, 10];
+
+console.log('semthis -> qualquer item * 2', mapSemThis(array));
